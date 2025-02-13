0118370f09486cd59e573671dbfe76f2e6731761
Kiyegga Francis <kiyeggafrancis1@gmail.com>
2025-02-13 20:57:38 +0300

    Initial Commit

---
+++ iTour App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour App
+//
+//  Created by kiyegga francis on 2025-02-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTour App/iTour_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTour_AppApp.swift
+//  iTour App
+//
+//  Created by kiyegga francis on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct iTour_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
