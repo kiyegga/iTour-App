37efefe4e1380494ecd2542614d689f91a962bf0
Kiyegga Francis <kiyeggafrancis1@gmail.com>
2025-02-18 21:44:51 +0300

    swiftData update on iTour app

---
+++ iTour App/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour App
+//
+//  Created by kiyegga francis on 2025-02-14.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
